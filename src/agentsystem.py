58040d22a2a7ae5c8ab87a4687724dc4767d832a
Spaklak <kru4aew.egor@yandex.ru>
2025-05-28 02:17:35 +0300

    done image generator

--- src/agentsystem.py
+++ src/agentsystem.py
@@ -1,6 +1,9 @@
 import logging
 import re
 
+import time
+import requests
+
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
@@ -13,6 +16,7 @@ class AgentSystem:
         model: str,
         base_url: str,
         api_key: str,
+        api_key_image: str,
         temperature: float = 0.7,
         top_p: float = 0.9,
     ):
@@ -23,6 +27,56 @@ class AgentSystem:
             temperature=temperature,
             top_p=top_p,
         )
+        self._api_key_image = api_key_image
+    
+    def create_image(self, prompt: str) -> str:
+        """Получает промт, а возвращает ссылку на картинку"""
+        url = "https://api.freepik.com/v1/ai/mystic"
+        payload = {
+        "prompt": prompt,
+        "structure_strength": 50,
+        "adherence": 50,
+        "hdr": 50,
+        "resolution": "1k",
+        "aspect_ratio": "social_story_9_16",
+        "model": "realism",
+        "creative_detailing": 33,
+        "engine": "automatic",
+        "fixed_generation": False,
+        "filter_nsfw": True,
+        }
+        headers = {
+            "x-freepik-api-key": self._api_key_image,
+            "Content-Type": "application/json"
+        }
+        response = requests.request("POST", url, json=payload, headers=headers)
+        if response.status_code == 200:
+            id = response.json()['data']['task_id']
+        else:
+            raise "Картинка не создалась"
+        for _ in range(3):
+            time.sleep(5)
+            url = f"https://api.freepik.com/v1/ai/mystic/{id}"
+            response = requests.request("GET", url, headers=headers)
+            if response.json()['data']['status'] == 'COMPLETED':
+                return (response.json()['data']['generated'])
+        raise "Не вышло найти картинку"
+    
+    def get_summary_history(self, history: str) -> str:
+        template = """Ты - профессиональный литератор
+        Тебе нужно из следующего текста выделить какой-то момент, чтобы потом на основании этого момента можно было сделать картину. Так что сделай акцент на том, что на картине должен быть отображен человек либо люди, которые участвуют в выбранном моменте. 
+        Однако имена людей не нужно указывать. Можешь просто написать, что это "советский солдат", если он таким является, но не имена.
+        На задание у тебя есть 200 символов. Текст должен быть на английском
+
+        Текст, с которым надо работать:
+        {history}
+
+        В качестве ответа напиши только текст на английском. Не нужно никаких дополнительных фраз и слов
+        """
+        prompt = PromptTemplate.from_template(template)
+        chat = prompt | self.model
+        response = chat.invoke({"history": history})
+        return response.content
 
     def _check_user_query(self, query: str) -> bool:
         """Проверяет запрос пользователя на соответствие требованиями военной тематики."""
@@ -136,7 +190,7 @@ class AgentSystem:
         prompt = PromptTemplate.from_template(template)
         chat = prompt | model
         response = chat.invoke({"text": letter})
-        extracted_emotions = self.extract_emotions_from_llm_response(response.content)
+        extracted_emotions = self._extract_emotions_from_llm_response(response.content)
         return extracted_emotions if extracted_emotions != "модель не ответила" else " "
 
     def process_agent_system(self, query: str = None, letter: str = None) -> str:
@@ -176,6 +230,8 @@ class AgentSystem:
         
         В качестве ответа ты должен только написать историю, которую ты сочинил. История должна быть до 500 слов, но не менее 300.
         """
+        if query is None and letter is None:
+            return "Ваш запрос не содержит ни запроса, ни письма. Введите что-нибудь"
         if query is not None:
             is_normal_query = self._check_user_query(query)
             if not is_normal_query:
@@ -191,15 +247,23 @@ class AgentSystem:
             else:
                 emotions = self._analyze_emotions(self.model, letter)
         logger.info("Эмоции, которые мы получили: {emotions}")
-        content = chat.invoke(
-            {
-                "emotional": emotions,
-                "query": query if query else "",
-                "letter": letter if letter else "",
-            }
-        )
-        return (
-            content.content
-            if content
-            else "Сервис временно недоступен, попробуйте позже."
-        )
+        try:
+            history = chat.invoke(
+                {
+                    "emotional": emotions,
+                    "query": query if query else "",
+                    "letter": letter if letter else "",
+                }
+            )
+        except Exception as e:
+            logger.error(e)
+            return "Сервис временно недоступен, попробуйте позже."
+        
+        if history is None:
+            return "Сервис временно недоступен, попробуйте позже."
+        
+        # TODO обработать ошибки
+        history_summary = self.get_summary_history(history.content)
+        history_summary += "It all happened during WWII"
+        url = self.create_image(history_summary)
+        return {"history": history.content, "url_pic": url}
